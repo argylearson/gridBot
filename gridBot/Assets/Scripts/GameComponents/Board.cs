9340332b0f2a2ccbaef38ce0ea837ca7799264d2
Richard Lemke <rlemke@indiana.edu>
2017-11-05 11:23:47 -0500

    Project Upload
    Here's the beginning of the project. There's still a lot of work to do
    before our meeting on 11/6/17, but I have to leave the house for a bit.
    Will pick up later today.

---
+++ gridBot/Assets/Scripts/GameComponents/Board.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour{
+
+    [Range(1,10)]
+    public int width;
+    [Range(1,10)]
+    public int height;
+    public Edge edge;
+
+    /*represents all of the vertical edges in the board
+    uses [x,y] notation to indicate the top vertex of
+    the line. For example, vertEdges[0,0] is the edge
+    descending from the top left node. vertEdges[1,0] 
+    is the next edge to the right.*/
+    public Edge[,] vertEdges;
+    /*represents the horizontal edges in the board.
+    horzEdges[0,0] is the top left horizontal line,
+    and horzEdges[1,0] is the next line to the right.
+    */
+    public Edge[,] horzEdges;
+    /* Here's how the board indices work
+     *  (0,0) - (1,0) - (2,0)
+     *    |       |       |
+     *  (0,1) - (1,1) - (1,2)
+     *    |       |       |
+     *  (0,2) - (1,2) - (2,2)
+    */
+
+
+    private void Start()
+    {
+        vertEdges = new Edge[width, height];
+        horzEdges = new Edge[width, height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                vertEdges[i, j] = Instantiate(edge, new Vector3(i, j, 0), Quaternion.Euler(90f, 0f, 0f), this.transform);
+                vertEdges[i, j].transform.name = "vert: " + i + ", " + j;
+                horzEdges[i, j] = Instantiate(edge, new Vector3(i, j, 0), Quaternion.identity, this.transform);
+                horzEdges[i, j].transform.name = "horz: " + i + ", " + j;
+            }
+        }
+    }
+}
---
+++ gridBot/Assets/Scripts/GameComponents/Edge.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Edge : MonoBehaviour {
+
+    #region fields and properties
+    public IPlayer owner;
+    private SpriteRenderer sprite;
+    public readonly int x;
+    public readonly int y;
+    public readonly EdgeDirection edgeDirection;
+    private int _traversals;
+
+    public int Traversals
+    {
+        get { return _traversals; }
+    }
+    #endregion
+
+    #region constructors
+    public Edge (int x, int y, EdgeDirection edgeDirection)
+    {
+        this.x = x;
+        this.y = y;
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+
+        sprite.color = Color.gray;
+        sprite.size = new Vector2(1, 1);
+        sprite.transform.parent = this.transform;
+        transform.position = new Vector3Int(x,y,0);
+    }
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
