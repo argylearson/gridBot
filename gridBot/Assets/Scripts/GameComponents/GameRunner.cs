dceff91cead11fd7d9398caee20433a05b9d1786
Richard Lemke <rlemke@indiana.edu>
2017-11-15 19:34:48 -0500

    Smooth Movement

--- gridBot/Assets/Scripts/GameComponents/GameRunner.cs
+++ gridBot/Assets/Scripts/GameComponents/GameRunner.cs
@@ -27,11 +27,11 @@ public class GameRunner : MonoBehaviour {
                     state.player = player.AddComponent<KeyboardPlayer>();
                     state.x = 0;
                     state.y = 0;
-                    state.startPosition = new Vector3(0,0);
+                    state.startPosition = new Vector3(-.1f,0);
                     state.endPosition = state.startPosition;
                     players.Add(state);
                     player.AddComponent<SpriteRenderer>().sprite = playerSprite;
-                    Instantiate(player, new Vector3(-.1f, 0), Quaternion.identity, this.transform);
+                    //Instantiate(player, new Vector3(-.1f, 0), Quaternion.identity, this.transform);
                     break;
             }
         }
@@ -60,9 +60,29 @@ public class GameRunner : MonoBehaviour {
     {
         for (int i = 0; i < playerTypes.Length; i++)
         {
-            players[i].player.transform.position = Vector3.Lerp(players[i].startPosition, players[i].endPosition, Time.time - players[i].startTime);
+            players[i].player.transform.position = Vector3.Lerp(players[i].startPosition, players[i].endPosition, (Time.time - players[i].startTime)*2);
         }
         var move = players[0].player.MakeMove(board, 0f);
-        if (move != null) RecolorEdge(move);
+        if (move != null)
+        {
+            players[0].startPosition = players[0].endPosition;
+            players[0].startTime = Time.time;
+            switch(move.direction)
+            {
+                case EdgeDirection.Up:
+                    players[0].endPosition = new Vector3(move.x - .1f, move.y + 1);
+                    break;
+                case EdgeDirection.Down:
+                    players[0].endPosition = new Vector3(move.x - .1f, move.y - 1);
+                    break;
+                case EdgeDirection.Left:
+                    players[0].endPosition = new Vector3(move.x - 1.1f, move.y);
+                    break;
+                case EdgeDirection.Right:
+                    players[0].endPosition = new Vector3(move.x + .9f, move.y);
+                    break;
+            }
+            RecolorEdge(move);
+        }
     }
 }
